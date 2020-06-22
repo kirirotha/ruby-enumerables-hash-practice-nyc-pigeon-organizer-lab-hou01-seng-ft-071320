c4be1d27714badebd52e4d1a7beab848b097dc30
Kiri Um <kirirotha@gmail.com>
2020-06-22 13:07:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -40,6 +40,7 @@ def nyc_pigeon_organizer(data)
         #puts value2
         value2.each do |key3, value3|
           puts key3.to_s + " " + key2.to_s + " " + key1.to_s
+          data2 << key3[key1][key2]
         end
       end
     end
