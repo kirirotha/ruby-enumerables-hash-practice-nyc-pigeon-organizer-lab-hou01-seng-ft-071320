4fc729b93f0129eca88435d3fab321127f2f4daa
Kiri Um <kirirotha@gmail.com>
2020-06-22 12:14:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = []
+  data2 = []
+  data3 = []
+    data.each do |key1, value1|
+      key1 = 
+      data2 << data[key1]
+      data2.each do |key2|
+        data3 << data[key2]
+        
+      end
+    end
+  puts data
+    
+  
 end
+
+
+nyc_pigeon_organizer(pigeon_data)
